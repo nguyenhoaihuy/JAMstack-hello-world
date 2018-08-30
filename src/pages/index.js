68aba7bf4109897c13e5eef540c59fa84559aa97
Huy Nguyen <nguyenhoaihuy92@gmail.com>
2018-08-29 22:16:38 -0700

    customize the introduction

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <h1>Hi everyone</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>My name is Huy Nguyen. I'm curently a student in Evergreen Valley College. I love coding, and I can be in the front of the computer hours</p>
+    <p>My name is Huy Nguyen. I am curently a student in Evergreen Valley College. I love coding, and I can be in the front of the computer hours</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
