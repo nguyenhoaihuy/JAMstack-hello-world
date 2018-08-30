f389cdf333f3e32b1d78896e158007b359534101
Huy Nguyen <nguyenhoaihuy92@gmail.com>
2018-08-29 22:14:50 -0700

    customize the introduction

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
+    <h1>Hi everyone</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>My name is Huy Nguyen. I'm curently a student in Evergreen Valley College. I love coding, and I can be in the front of the computer hours</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
